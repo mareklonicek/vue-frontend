a47516c4360f053156787ecfdfd9bf8bb024d410
Marek Loníček <mark.lonicek@seznam.cz>
2024-12-20 15:55:47 +0100

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Seznam uživatelů</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.name }} ({{ user.email }})
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue';
+
+export default {
+  setup() {
+    const users = ref([]);
+
+    onMounted(async () => {
+      const response = await fetch('http://localhost:8000/api/users');
+      users.value = await response.json();
+    });
+
+    return { users };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  font-family: Arial, sans-serif;
+  color: #333;
+}
+</style>
